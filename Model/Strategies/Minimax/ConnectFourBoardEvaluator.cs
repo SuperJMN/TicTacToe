7d117abb39850b01200d36e72b3947aca987b02a
SuperJMN <superjmn@outlook.com>
2014-05-23 13:08:26 +0200

    First steps implementing heuristic for C4

--- Model/Strategies/Minimax/ConnectFourBoardEvaluator.cs
+++ Model/Strategies/Minimax/ConnectFourBoardEvaluator.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Model.Strategies.Minimax
 {
     public class ConnectFourBoardEvaluator : IBoardEvaluator
@@ -6,12 +10,48 @@ namespace Model.Strategies.Minimax
 
         public ConnectFourBoardEvaluator(ConnectFourBoard board)
         {
-            this.board = board;            
+            this.board = board;
         }
 
         public int Evaluate(Player player)
         {
+            var validPositions = board.GetValidMovePositions();
+
+            var scores = from position in validPositions
+                         select new
+                         {
+                             Position = position,
+                             Score = EvaluatePosition(player, position)
+                         };
+
+            var positionEvaluation = scores.Max();
+
+            return positionEvaluation.Score;
+        }
+
+        private int EvaluatePosition(Player player, Position position)
+        {
+            var newBoard = board.Clone();
+            newBoard.Move(new Movement(position, player));
+
+            int rowScore = EvaluateRow(position, newBoard);
+            return rowScore;
+        }
+
+        private int EvaluateRow(Position position, Board newBoard)
+        {
+            var squares = GetRow(position, newBoard, 4);
             return 0;
+        }
+
+        private SquareList GetRow(Position position, Board newBoard, int i)
+        {
+            var start = Math.Max(position.X - i, 0);
+            var end = Math.Min(position.X + i, newBoard.Width - 1);
+            var count = board.Width - start - end;
+
+            var squares = newBoard.Rows[position.Y].GetRange(start, count);
+            return new SquareList(squares);
         }
     }
 }
